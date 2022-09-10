3557f88e379ee8898716de71e69969358ced2977
Jeremiah-Secrist <112994503+Jeremiah-Secrist@users.noreply.github.com>
2022-09-10 18:19:25 -0400

    update config

--- src/config.mjs
+++ src/config.mjs
@@ -1,11 +1,11 @@
 export const SITE = {
   name: "AstroWind",
 
-  origin: "https://astrowind.vercel.app",
+  origin: "https://jeremiah.secrist.xyz",
   basePathname: "/",
 
-  title: "AstroWind — Your website with Astro + Tailwind CSS",
-  description: "🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.",
+  title: "Jeremiah Secrist",
+  description: "A resume about myself.",
 
   googleAnalyticsId: false, // or "G-XXXXXXXXXX",
   googleSiteVerificationId: "orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M",
